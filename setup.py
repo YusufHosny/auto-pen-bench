f42e9407070076debdd1f463a2178ee39a5c1953
Yusuf Hosny <yusufyy2004@hotmail.com>
2025-12-26 00:45:59 +0100

    fixed version issues

--- setup.py
+++ setup.py
@@ -5,16 +5,17 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'python-dotenv==1.0.1',
-        'paramiko==3.5.0',
-        'termcolor==2.4.0',
-        'chardet==5.2.0',
-        'pydantic==2.9.2',
-        'ipykernel==6.29.5',
-        'pyyaml==6.0.2',
-        'openai==1.51.0',
-        'instructor==1.5.0',
-        'httpx==0.27.2'
+        'python-dotenv>=1.0.1',
+        'paramiko>=3.5.0',
+        'termcolor>=2.4.0',
+        'chardet>=5.2.0',
+        'pydantic>=2.9.2',
+        'ipykernel>=6.29.5',
+        'pyyaml>=6.0.2',
+        'openai>=1.51.0',
+        'instructor>=1.5.0',
+        'httpx>=0.28.1',
+        'tenacity>=9.1.2'
     ],
     include_package_data=True,
 )
